925df4b023939119037c47bd673a5fdc69376e93
SaniAdamou14 <sonicstark3@gmail.com>
2025-05-14 18:55:36 +0100

    ss

--- show_disk_usage.sh
+++ show_disk_usage.sh
@@ -4,12 +4,3 @@
 # Objectif : Affiche l'utilisation du disque pour un répertoire donné ou courant
 # Auteur : Mahamane Sani Adamou Mahamane
 
-# Affiche l'utilisation du disque (espace global)
-echo "Utilisation du disque (globale) :"
-df -h 2>/dev/null || echo "Commande df non disponible."
-
-echo ""
-
-# Affiche l’utilisation du répertoire courant
-echo "Utilisation du disque (répertoire courant) :"
-du -sh . 2>/dev/null || echo "Commande du non disponible."
