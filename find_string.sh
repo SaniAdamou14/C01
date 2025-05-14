37d5460f12bd7acf2bc3cdccff47472f8369d9a8
SaniAdamou14 <sonicstark3@gmail.com>
2025-05-14 19:08:25 +0100

    Test2

--- find_string.sh
+++ find_string.sh
@@ -4,16 +4,16 @@
 # Objectif : Rechercher une chaîne littérale dans un fichier
 # Auteur : Mahamane Sani Adamou Mahamane
 
-read file
-read search
+# read file
+# read search
 
-if [ ! -f "$file" ]; then
-    echo "Le fichier $file n'existe pas."
-    exit 1
-fi
+# if [ ! -f "$file" ]; then
+#     echo "Le fichier $file n'existe pas."
+#     exit 1
+# fi
 
-if grep -q "$search" "$file"; then
-    echo "La chaîne '$search' a été trouvée dans $file."
-else
-    echo "La chaîne '$search' n'a pas été trouvée dans $file."
-fi
+# if grep -q "$search" "$file"; then
+#     echo "La chaîne '$search' a été trouvée dans $file."
+# else
+#     echo "La chaîne '$search' n'a pas été trouvée dans $file."
+# fi
