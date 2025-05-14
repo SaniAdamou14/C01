bba7e22fa91fbe81f7776396c8d0169d2cc97587
SaniAdamou14 <sonicstark3@gmail.com>
2025-05-14 19:09:14 +0100

    Test3

--- count_files.sh
+++ count_files.sh
@@ -4,12 +4,12 @@
 # Objectif : Compter le nombre de fichiers réguliers dans un répertoire
 # Auteur : Mahamane Sani Adamou Mahamane
 
-read folder
+# read folder
 
-if [ -d "$folder" ]; then
-    count=$(find "$folder" -type f | wc -l)
-    echo "Le dossier $folder contient $count fichier(s)."
-else
-    echo "Le dossier $folder n'existe pas."
-    exit 1
-fi
+# if [ -d "$folder" ]; then
+#     count=$(find "$folder" -type f | wc -l)
+#     echo "Le dossier $folder contient $count fichier(s)."
+# else
+#     echo "Le dossier $folder n'existe pas."
+#     exit 1
+# fi
